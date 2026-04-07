27012affd646dc4420062f2823a9bdf6a0cb39da
yunyounglee99 <kembel0116@gmail.com>
2026-04-07 14:45:48 +0900

    modi test_robot.py for testing training

--- test_robot.py
+++ test_robot.py
@@ -5,7 +5,22 @@ import nexodim as nxd
 
 
 robot = nxd.robots.SO101()
+policy = nxd.policies.vla.SmolVLA()
 
+robot.connect()
+policy.load_policy(
+    task="pick up the cup"
+)
+policy.set_dataset_features(robot)
+obs = robot.get_observation()
+action = policy.inference_policy(obs)
+robot.send_action(action)
+print(f'Action : {action}')
+
+robot.disconnect()
+
+"""
 robot.connect(mode="teach")
 robot.record(task='pick up', episodes=3)
 robot.disconnect()
+"""
